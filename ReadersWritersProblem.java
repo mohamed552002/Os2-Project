f40dff6d04e1156bcaa74988e1e58fab7f180d7a
Naguib <118809711+Mohamed-Naguib22@users.noreply.github.com>
2022-11-25 21:44:17 +0200

    Update ReadersWritersProblem.java

--- ReadersWritersProblem.java
+++ ReadersWritersProblem.java
@@ -1,51 +1,10 @@
 package ReadersWritersProblem;
 import java.util.concurrent.Semaphore;
-
+import java.io.*;
 class ReadersWritersProblem {
-
-    static Semaphore readLock = new Semaphore(1); //mutex = 1
-    static Semaphore writeLock = new Semaphore(1); // rw_mutex = 1
+    static Semaphore readLock = new Semaphore(1); 
     static int readCount = 0; // how many processes reading object
-
-    static class Read implements Runnable { //Reading Process
-        @Override
-        public void run() {
-            try {
-                readLock.acquire(); //wait(mutex)
-                readCount++;    
-                if (readCount == 1) {
-                    writeLock.acquire(); //wait(rw_mutex)
-                }
-                readLock.release(); //signal(mutex)
-                
-                System.out.println("Thread " + Thread.currentThread().getName() + " is reading");
-                Thread.sleep(2000);
-                System.out.println("Thread " + Thread.currentThread().getName() + " has finished reading");
-
-                readLock.acquire(); //wait(mutex)
-                readCount--;
-                if(readCount == 0) {
-                    writeLock.release(); //signal(rw_mutex)
-                }
-                readLock.release(); //signal(mutex)
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    static class Write implements Runnable { // Writing Process
-        @Override
-        public void run() {
-            try {
-                writeLock.acquire(); //wait(rw_mutex)
-                System.out.println("Thread " + Thread.currentThread().getName() + " is waiting");
-                Thread.sleep(2000);
-                System.out.println("Thread " + Thread.currentThread().getName() + " has finished waiting");
-                writeLock.release(); //signal(rw_mutex)
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
+    static Semaphore writeLock = new Semaphore(1); 
+    static String path = "test.txt";
+    static File file = new File(path);
 }
