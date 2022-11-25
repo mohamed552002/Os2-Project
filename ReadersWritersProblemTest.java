72ee1bd9729da1d54a3fa31be649ecdbb335791b
Naguib <118809711+Mohamed-Naguib22@users.noreply.github.com>
2022-11-25 21:44:53 +0200

    Update ReadersWritersProblemTest.java

--- ReadersWritersProblemTest.java
+++ ReadersWritersProblemTest.java
@@ -1,20 +1,20 @@
 package ReadersWritersProblem;
-
 public class ReadersWritersProblemTest {
     public static void main(String[] args) throws Exception {
-        ReadersWritersProblem.Read read = new ReadersWritersProblem.Read();
-        ReadersWritersProblem.Write write = new ReadersWritersProblem.Write();
-        Thread t1 = new Thread(write);
+        
+        Reader r = new Reader();
+        Writer w = new Writer();
+            
+        Thread t1 = new Thread(r);
         t1.setName("thread1");
-        Thread t2 = new Thread(write);
+        Thread t2 = new Thread(r);
         t2.setName("thread2");
-        Thread t3 = new Thread(read);
-        t3.setName("thread3");
-        Thread t4 = new Thread(read);
-        t4.setName("thread4");
+        Thread t3 = new Thread(r);
+        t3.setName("thread2");
+        
         t1.start();
-        t3.start();
         t2.start();
-        t4.start();
+        //t3.start();
+        
     }
 }
